77b70d95317a3f514fc4f31938f569f007d89ca3
KaylaFernandez626 <104842049+KaylaFernandez626@users.noreply.github.com>
2022-05-03 09:55:48 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".puppy-poster").dblclick(function() {
+$(".puppy-poster").show();
+$("h1").hide();    
+$("p").hide();
+$("img").attr("src", "https://i.pinimg.com/236x/ec/54/ed/ec54ed17d25c936c6fd5346f1c1cc2cd.jpg");
+$(".park").show();
+$(".store").show();
+});
+
